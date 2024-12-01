5eb4f1de0cb702f303cb46dd90d1da4af75a7007
Wnnk <1275056222@qq.com>
2024-12-01 21:04:53 +0800

    <feat> 调色板创建

--- src/components/SideBarRight/ColorPick.vue
+++ src/components/SideBarRight/ColorPick.vue
@@ -1,55 +1,196 @@
-<script setup lang='ts'>
-import { computed, ref } from 'vue'
+<script setup lang="ts">
+import { computed, ref, watch, onMounted } from "vue";
+import {
+  hslToRgb,
+  rgbToHex,
+  hslToHex,
+  rgbToHsl,
+  hsvToRgb,
+  hexToRgb,
+} from "../../utils/color";
+
+onMounted(() => {
+  const color = "#a55959";
+  /* hexToRgb √ */
+});
+
 const buttonGroup = ref([true, true, true]);
 
 const initSwatches = () => {
   const swatches = [];
   for (let i = 0; i < 21; i++) {
     swatches.push({
-      color: 'grb(255, 255, 255)'
+      color: "grb(255, 255, 255)",
     });
   }
   return swatches;
-}
+};
 const swatches = ref(initSwatches());
 const swatchActive = ref(0);
+const hex = ref("#ffffff");
+const rgb = ref({ r: 255, g: 255, b: 255 });
+const hsl = ref<{ h: number; s: number; l: number }>({ h: 0, s: 100, l: 100 });
 
 const inputGroup = ref([
-  {label: 'rgb_r_label', title: 'Red', class: 'text_red', rangeId: 'rgb_r_range', trackBackground: 'background: linear-gradient(to right, rgb(0, 255, 255), rgb(255, 255, 255));', inputId: 'rgb_r', value: 255, text: 'Red'},
-  {label: 'rgb_g_label', title: 'Green', class: 'text_green', rangeId: 'rgb_g_range', trackBackground: 'background: linear-gradient(to right, rgb(255, 0, 255), rgb(255, 255, 255));', inputId: 'rgb_g', value: 155, text: 'Green'},
-  {label: 'rgb_b_label', title: 'Blue', class: 'text_blue', rangeId: 'rgb_b_range', trackBackground: 'background: linear-gradient(to right, rgb(255, 255, 0), rgb(255, 255, 255));', inputId: 'rgb_b', value: 55, text: 'Blue'},
-])
+  {
+    label: "rgb_r_label",
+    title: "Red",
+    class: "text_red",
+    rangeId: "rgb_r_range",
+    trackBackground:
+      "background: linear-gradient(to right, rgb(0, 255, 255), rgb(255, 255, 255));",
+    inputId: "rgb_r",
+    value: rgb.value.r,
+    text: "Red",
+  },
+  {
+    label: "rgb_g_label",
+    title: "Green",
+    class: "text_green",
+    rangeId: "rgb_g_range",
+    trackBackground:
+      "background: linear-gradient(to right, rgb(255, 0, 255), rgb(255, 255, 255));",
+    inputId: "rgb_g",
+    value: rgb.value.g,
+    text: "Green",
+  },
+  {
+    label: "rgb_b_label",
+    title: "Blue",
+    class: "text_blue",
+    rangeId: "rgb_b_range",
+    trackBackground:
+      "background: linear-gradient(to right, rgb(255, 255, 0), rgb(255, 255, 255));",
+    inputId: "rgb_b",
+    value: rgb.value.b,
+    text: "Blue",
+  },
+]);
 
+const colorPickStyle = ref({
+  left: "0",
+  top: "0",
+});
 
+const barStyle = ref({
+  height: "100%",
+});
+
+const hsv = ref({
+  h: 0,
+  s: 1,
+  v: 1,
+});
+
+const secondaryPickStyle = ref({
+  background: "rgb(255, 0, 0)",
+});
+
+const colorPick = (event: MouseEvent) => {
+  const picker = document.getElementsByClassName("secondary_pick");
+
+  const rect = picker[0]!.getBoundingClientRect();
+  const offsetX = event.clientX - rect.left;
+  const offsetY = event.clientY - rect.top;
+  const x = Math.floor((offsetX / rect.width) * 100);
+  const y = Math.floor((offsetY / rect.height) * 100);
+  colorPickStyle.value = {
+    left: `${x}%`,
+    top: `${y}%`,
+  };
+  hsv.value.s = x / 100;
+  hsv.value.v = (100 - y) / 100;
+  const { r, g, b } = hslToRgb(hsv.value.h, hsv.value.s, hsv.value.v);
+  hex.value = rgbToHex(r, g, b);
+  const hsl = hslToHex(hsv.value.h, hsv.value.s, hsv.value.v);
+  const { h, s, l } = rgbToHsl(r, g, b);
+};
+
+/**
+ * @description: 调整色相柱
+ */
+const adjustHue = (event: MouseEvent) => {
+  const picker = document.getElementsByClassName("primary_pick");
+  const rect = picker[0]!.getBoundingClientRect();
+  const offsetY = event.clientY - rect.top;
+
+  const y = Math.max(0, Math.min(100, 100 - (offsetY / rect.height) * 100));
+  barStyle.value = {
+    height: `${y}%`,
+  };
+  hsv.value.h = Math.floor((offsetY / rect.height) * 360);
+
+  const { r, g, b } = hsvToRgb(hsv.value.h, 1, 0.5);
+  secondaryPickStyle.value.background = `rgb(${r}, ${g}, ${b})`;
+  console.log(hsv.value.h, secondaryPickStyle.value.background);
+};
 </script>
 
 <template>
   <div class="colors block">
-    <h2 class="trn toggle">
-      颜色
-    </h2>
+    <h2 class="trn toggle">颜色</h2>
     <div class="content" id="toggle_colors">
       <div class="ui_flex_group justify_content_space_between stacked">
-        <div id="selected_color_sample" class="ui_color_sample" title="Current Color Preview"></div>
+        <div
+          id="selected_color_sample"
+          class="ui_color_sample"
+          title="Current Color Preview"
+        ></div>
         <div class="ui_button_group">
           <!-- Color Picker -->
-          <button id="toggle_color_picker_section_button" title="Toggle Color Picker" :aria-pressed="buttonGroup[0]">
+          <button
+            id="toggle_color_picker_section_button"
+            title="Toggle Color Picker"
+            :aria-pressed="buttonGroup[0]"
+          >
             <span class="sr_only">Toggle Color Picker</span>
-            <svg width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <rect width="24" height="24" opacity="0"></rect>
-              <path d="M19.54 5.08A10.61 10.61 0 0 0 11.91 2a10 10 0 0 0-.05 20 2.58 2.58 0 0 0 2.53-1.89 2.52 2.52 0 0 0-.57-2.28.5.5 0 0 1 .37-.83h1.65A6.15 6.15 0 0 0 22 11.33a8.48 8.48 0 0 0-2.46-6.25zM15.88 15h-1.65a2.49 2.49 0 0 0-1.87 4.15.49.49 0 0 1 .12.49c-.05.21-.28.34-.59.36a8 8 0 0 1-7.82-9.11A8.1 8.1 0 0 1 11.92 4H12a8.47 8.47 0 0 1 6.1 2.48 6.5 6.5 0 0 1 1.9 4.77A4.17 4.17 0 0 1 15.88 15z"></path>
+              <path
+                d="M19.54 5.08A10.61 10.61 0 0 0 11.91 2a10 10 0 0 0-.05 20 2.58 2.58 0 0 0 2.53-1.89 2.52 2.52 0 0 0-.57-2.28.5.5 0 0 1 .37-.83h1.65A6.15 6.15 0 0 0 22 11.33a8.48 8.48 0 0 0-2.46-6.25zM15.88 15h-1.65a2.49 2.49 0 0 0-1.87 4.15.49.49 0 0 1 .12.49c-.05.21-.28.34-.59.36a8 8 0 0 1-7.82-9.11A8.1 8.1 0 0 1 11.92 4H12a8.47 8.47 0 0 1 6.1 2.48 6.5 6.5 0 0 1 1.9 4.77A4.17 4.17 0 0 1 15.88 15z"
+              ></path>
               <circle cx="12" cy="6.5" r="1.5"></circle>
-              <path d="M15.25 7.2a1.5 1.5 0 1 0 2.05.55 1.5 1.5 0 0 0-2.05-.55z"></path>
-              <path d="M8.75 7.2a1.5 1.5 0 1 0 .55 2.05 1.5 1.5 0 0 0-.55-2.05z"></path>
-              <path d="M6.16 11.26a1.5 1.5 0 1 0 2.08.4 1.49 1.49 0 0 0-2.08-.4z"></path>
-				    </svg>
+              <path
+                d="M15.25 7.2a1.5 1.5 0 1 0 2.05.55 1.5 1.5 0 0 0-2.05-.55z"
+              ></path>
+              <path
+                d="M8.75 7.2a1.5 1.5 0 1 0 .55 2.05 1.5 1.5 0 0 0-.55-2.05z"
+              ></path>
+              <path
+                d="M6.16 11.26a1.5 1.5 0 1 0 2.08.4 1.49 1.49 0 0 0-2.08-.4z"
+              ></path>
+            </svg>
           </button>
           <!-- Color Channels -->
-          <button id="toggle_color_channels_section_button" :aria-pressed="buttonGroup[1]" class="ui_icon_button" title="Toggle Color Channels">
+          <button
+            id="toggle_color_channels_section_button"
+            :aria-pressed="buttonGroup[1]"
+            class="ui_icon_button"
+            title="Toggle Color Channels"
+          >
             <span class="sr_only">Toggle Color Channels</span>
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"></path>
-              <path fill-rule="evenodd" d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z"></path>
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-card-list"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+              ></path>
+              <path
+                fill-rule="evenodd"
+                d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z"
+              ></path>
               <circle cx="3.5" cy="5.5" r=".5"></circle>
               <circle cx="3.5" cy="8" r=".5"></circle>
               <circle cx="3.5" cy="10.5" r=".5"></circle>
@@ -57,21 +198,41 @@ const inputGroup = ref([
           </button>
 
           <!-- Swatches -->
-          <button id="toggle_color_swatches_section_button" :aria-pressed="buttonGroup[2]" class="ui_icon_button" title="Toggle Swatches">
+          <button
+            id="toggle_color_swatches_section_button"
+            :aria-pressed="buttonGroup[2]"
+            class="ui_icon_button"
+            title="Toggle Swatches"
+          >
             <span class="sr_only">Toggle Swatches</span>
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-grid-3x2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v8a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5v-8zM1.5 3a.5.5 0 0 0-.5.5V7h4V3H1.5zM5 8H1v3.5a.5.5 0 0 0 .5.5H5V8zm1 0h4v4H6V8zm4-1H6V3h4v4zm1 1v4h3.5a.5.5 0 0 0 .5-.5V8h-4zm0-1V3h3.5a.5.5 0 0 1 .5.5V7h-4z"></path>
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-grid-3x2"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v8a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5v-8zM1.5 3a.5.5 0 0 0-.5.5V7h4V3H1.5zM5 8H1v3.5a.5.5 0 0 0 .5.5H5V8zm1 0h4v4H6V8zm4-1H6V3h4v4zm1 1v4h3.5a.5.5 0 0 0 .5-.5V8h-4zm0-1V3h3.5a.5.5 0 0 1 .5.5V7h-4z"
+              ></path>
             </svg>
           </button>
         </div>
       </div>
 
-      <div id="color_section_swatches" class="block_section" v-show="buttonGroup[2]">
+      <div
+        id="color_section_swatches"
+        class="block_section"
+        v-show="buttonGroup[2]"
+      >
         <div class="ui_swatches" id="color_swatches">
           <div class="swatch_group cols_7 rows_3" tabindex="0">
-            <div 
-              :class="['swatch', {'active': swatchActive === index} ]" 
-              v-for="(swatch, index) in swatches" :key="index" 
+            <div
+              :class="['swatch', { active: swatchActive === index }]"
+              v-for="(swatch, index) in swatches"
+              :key="index"
               :style="`background-color: ${swatch.color};`"
               @click="swatchActive = index"
             ></div>
@@ -79,54 +240,126 @@ const inputGroup = ref([
         </div>
       </div>
 
-      <div id="color_section_picker" class="block_section" v-show="buttonGroup[0]">
-          <div class="ui_color_picker_gradient" id="color_picker_gradient">
-            <div class="secondary_pick">
-              <div class="saturation_gradient"></div>
-              <div class="value_gradient"></div>
-              <div class="handle" ></div>
-            </div>
-            <div class="primary_pick">
-              <div class="ui_range color_picker_thin vertical">
-                <div class="padded_track" style="background: linear-gradient(rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%);"></div>
-                <div class="bar">
-                  <div class="handle"></div>
-                </div>
+      <div
+        id="color_section_picker"
+        class="block_section"
+        v-show="buttonGroup[0]"
+      >
+        <div class="ui_color_picker_gradient" id="color_picker_gradient">
+          <div
+            class="secondary_pick"
+            @click="colorPick"
+            :style="secondaryPickStyle"
+          >
+            <div class="saturation_gradient"></div>
+            <div class="value_gradient"></div>
+            <div class="handle" :style="colorPickStyle"></div>
+          </div>
+          <div class="primary_pick" @click="adjustHue">
+            <div
+              class="ui_range color_picker_thin vertical"
+              tabindex="0"
+              role="slider"
+              aria-valuemin="0"
+              aria-valuemax="360"
+              :aria-valuenow="hsv.h"
+            >
+              <div
+                class="padded_track"
+                style="
+                  background: linear-gradient(
+                    rgb(255, 0, 0) 0%,
+                    rgb(255, 255, 0) 17%,
+                    rgb(0, 255, 0) 33%,
+                    rgb(0, 255, 255) 50%,
+                    rgb(0, 0, 255) 67%,
+                    rgb(255, 0, 255) 83%,
+                    rgb(255, 0, 0) 100%
+                  );
+                "
+              ></div>
+              <div class="bar" :style="barStyle">
+                <div class="handle"></div>
               </div>
             </div>
           </div>
-          <div class="ui_input_group stacked">
-            <label id="color_hex_label" title="Hex" class="label_width_small trn">Hex</label>
-            <input id="color_hex" value="#000000" maxlength="7" type="text">
-          </div>
         </div>
+        <div class="ui_input_group stacked">
+          <label id="color_hex_label" title="Hex" class="label_width_small trn"
+            >Hex</label
+          >
+          <input id="color_hex" :value="hex" maxlength="7" type="text" />
+        </div>
+      </div>
     </div>
 
-
-    <div id="color_section_channels" class="block_section color_section_channels">
+    <div
+      id="color_section_channels"
+      class="block_section color_section_channels"
+    >
       <div class="ui_input_grid stacked">
-        <div class="ui_input_group" v-for="input in inputGroup" :key="input.inputId">
-          <label :id="input.label" :title="input.title" :class="['label_width_character', input.class]">
-            <strong>{{input.text[0]}}<span class="sr_only">{{input.text.substring(1)}}</span></strong>
+        <div
+          class="ui_input_group"
+          v-for="input in inputGroup"
+          :key="input.inputId"
+        >
+          <label
+            :id="input.label"
+            :title="input.title"
+            :class="['label_width_character', input.class]"
+          >
+            <strong
+              >{{ input.text[0]
+              }}<span class="sr_only">{{
+                input.text.substring(1)
+              }}</span></strong
+            >
           </label>
-          <div class="ui_range color_picker" tabindex="0" role="slider" aria-valuemin="0" aria-valuemax="255" :aria-valuenow="input.value" :id="input.rangeId">
-            <div class="padded_track" style="background: linear-gradient(to right, rgb(0, 143, 136), rgb(255, 143, 136));"></div>
-            <div class="bar" style="width:33.3333%;">
+          <div
+            class="ui_range color_picker"
+            tabindex="0"
+            role="slider"
+            aria-valuemin="0"
+            aria-valuemax="255"
+            :aria-valuenow="input.value"
+            :id="input.rangeId"
+          >
+            <div
+              class="padded_track"
+              style="
+                background: linear-gradient(
+                  to right,
+                  rgb(0, 143, 136),
+                  rgb(255, 143, 136)
+                );
+              "
+            ></div>
+            <div class="bar" style="width: 33.3333%">
               <div class="handle"></div>
             </div>
           </div>
-          <div class="ui_number_input input_cw_3" :id="input.inputId + '_range'" >
-            <input type="number" :aria-labelledby="input.label" max="255" step="1" :value="input.value">
-            <button class="increase_number" tabindex="-1"><span class="sr_only" >Increase</span></button>
-            <button class="decrease_number" tabindex="-1"><span class="sr_only">Decrease</span></button>
+          <div
+            class="ui_number_input input_cw_3"
+            :id="input.inputId + '_range'"
+          >
+            <input
+              type="number"
+              :aria-labelledby="input.label"
+              max="255"
+              step="1"
+              :value="rgb.r"
+            />
+            <button class="increase_number" tabindex="-1">
+              <span class="sr_only">Increase</span>
+            </button>
+            <button class="decrease_number" tabindex="-1">
+              <span class="sr_only">Decrease</span>
+            </button>
           </div>
         </div>
       </div>
     </div>
   </div>
-    
-
 </template>
 
-<style lang='scss' scoped>
-</style>
+<style lang="scss" scoped></style>
---
+++ src/utils/color.ts
@@ -0,0 +1,148 @@
+export const hslToHex = (h: number, s: number, l: number) => {
+  const rgb = hslToRgb(h, s, l);
+  return rgbToHex(rgb.r, rgb.g, rgb.b);
+};
+
+export const hslToRgb = (h: number, s: number, l: number) => {
+  let r, g, b;
+
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    let p = 2 * l - q;
+    r = hueToRgb(p, q, h + 1 / 3);
+    g = hueToRgb(p, q, h);
+    b = hueToRgb(p, q, h - 1 / 3);
+  }
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255),
+  };
+};
+
+export const hueToRgb = (p: number, q: number, t: number) => {
+  if (t < 0) {
+    t += 1;
+  }
+  if (t > 1) {
+    t -= 1;
+  }
+  if (t < 1 / 6) {
+    return p + (q - p) * 6 * t;
+  }
+  if (t < 1 / 2) {
+    return q;
+  }
+  if (t < 2 / 3) {
+    return p + (q - p) * (2 / 3 - t) * 6;
+  }
+  return p;
+};
+
+export const rgbToHex = (r: number, g: number, b: number) => {
+  if (r > 255 || g > 255 || b > 255) {
+    throw new Error("Invalid color component");
+  }
+  const tmp = ((r << 16) | (g << 8) | b).toString(16);
+  return "#" + ("000000" + tmp).slice(-6);
+};
+
+export const rgbToHsl = (r: number, g: number, b: number) => {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  let h,
+    s,
+    l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    let d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+      default:
+        h = 0;
+        break;
+    }
+    h /= 6;
+  }
+
+  return { h, s, l };
+};
+
+export const hsvToRgb = (h: number, s: number, v: number) => {
+  let r = 0,
+    g = 0,
+    b = 0;
+
+  let i = Math.floor(h * 6);
+  let f = h * 6 - i;
+  let p = v * (1 - s);
+  let q = v * (1 - f * s);
+  let t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0:
+      (r = v), (g = t), (b = p);
+      break;
+    case 1:
+      (r = q), (g = v), (b = p);
+      break;
+    case 2:
+      (r = p), (g = v), (b = t);
+      break;
+    case 3:
+      (r = p), (g = q), (b = v);
+      break;
+    case 4:
+      (r = t), (g = p), (b = v);
+      break;
+    case 5:
+      (r = v), (g = p), (b = q);
+      break;
+  }
+
+  return { r: r * 255, g: g * 255, b: b * 255 };
+};
+
+export function hexToRgb(hex: string) {
+  if (hex[0] === "#") hex = hex.substr(1);
+  if (hex.length === 3) {
+    const temp = hex; // 使用const，因为temp不会被重新赋值
+    hex = "";
+    // 使用正则获取匹配结果，temp此时是一个字符串数组
+    const matches = /^([a-f0-9])([a-f0-9])([a-f0-9])$/i.exec(temp)?.slice(1);
+    if (matches) {
+      for (let i = 0; i < 3; i++) {
+        // 使用let，因为i会改变
+        hex += matches[i] + matches[i];
+      }
+    }
+  }
+  const triplets = /^([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})/i
+    .exec(hex)
+    ?.slice(1); // 使用const
+  if (!triplets) {
+    throw new Error("Invalid hex color");
+  }
+  return {
+    r: parseInt(triplets[0], 16),
+    g: parseInt(triplets[1], 16),
+    b: parseInt(triplets[2], 16),
+    a: 255,
+  };
+}
