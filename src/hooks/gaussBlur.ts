c082e69f71230fc0e8dc3590239d33e217cac8df
Wnnk <1275056222@qq.com>
2025-01-01 20:24:35 +0800

    提交

---
+++ src/hooks/gaussBlur.ts
@@ -0,0 +1,121 @@
+import Konva from "konva";
+
+/**
+ * @description : 高斯模糊
+ * @param {Konva.Image} konvaImage : konva图片对象
+ * @param {imageData} image : 图片
+ * @param {number} sigma : 标准差
+ * @param {number} radius : 半径
+ */
+export const gaussBlur = (
+  konvaImage: Konva.Image,
+  image: ImageData,
+  sigma: number = 10,
+  radius: number = 10
+) => {
+  const stage = konvaImage.getStage()!;
+  if (!stage) {
+    console.error("Failed to get stage");
+    return;
+  }
+  const pos = stage.getPointerPosition();
+  if (!pos) return;
+  const xCenter = pos.x;
+  const yCenter = pos.y;
+
+  const pixes = image.data;
+  const width = image.width;
+  const height = image.height;
+  const gaussMatrix = [];
+  const a = 1 / (Math.sqrt(2 * Math.PI) * sigma);
+  const b = -1 / (2 * sigma * sigma);
+  let gaussSum = 0;
+
+  /* 生成高斯矩阵 */
+  for (let i = 0, x = -radius; x <= radius; x++, i++) {
+    const g = a * Math.exp(b * x * x);
+    gaussMatrix[i] = g;
+    gaussSum += g;
+  }
+
+  for (let i = 0, len = gaussMatrix.length; i < len; i++) {
+    gaussMatrix[i] /= gaussSum;
+  }
+
+  const B_List_LENGTH = 3;
+
+  /* 限定模糊的区域 */
+  const xStart = Math.max(0, xCenter - radius);
+  const xEnd = Math.min(width, xCenter + radius);
+  const yStart = Math.max(0, yCenter - radius);
+  const yEnd = Math.min(height, yCenter + radius);
+
+  console.log(
+    "centerX:",
+    xCenter,
+    "centerY:",
+    yCenter,
+    "xStart:",
+    xStart,
+    "xEnd:",
+    xEnd,
+    "yStart:",
+    yStart,
+    "yEnd:",
+    yEnd
+  );
+
+  /* x方向 */
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      const bList = new Array(B_List_LENGTH).fill(0);
+      gaussSum = 0;
+
+      for (let j = -radius; j < radius; j++) {
+        const k = x + j;
+        if (k >= 0 && k < width) {
+          const i = (y * width + k) * 4;
+          for (let l = 0; l < bList.length; l++) {
+            bList[l] += pixes[i + l] * gaussMatrix[j + radius];
+          }
+          gaussSum += gaussMatrix[j + radius];
+        }
+      }
+      const i = (y * width + x) * 4;
+      for (let l = 0; l < bList.length; l++) {
+        pixes[i + l] = Math.round(bList[l] / gaussSum);
+      }
+    }
+  }
+
+  /* y方向 */
+  for (let x = xStart; x < xEnd; x++) {
+    for (let y = yStart; y < yEnd; y++) {
+      const bList = new Array(B_List_LENGTH).fill(0);
+      gaussSum = 0;
+
+      for (let j = -radius; j < radius; j++) {
+        const k = y + j;
+        if (k >= 0 && k < height) {
+          const i = (k * width + x) * 4;
+          for (let l = 0; l < bList.length; l++) {
+            bList[l] += pixes[i + l] * gaussMatrix[j + radius];
+          }
+          gaussSum += gaussMatrix[j + radius];
+        }
+      }
+      const i = (y * width + x) * 4;
+      for (let l = 0; l < bList.length; l++) {
+        pixes[i + l] = Math.round(bList[l] / gaussSum);
+      }
+    }
+  }
+
+  const context = konvaImage.getLayer()!.getContext();
+  if (!context) {
+    console.error("Failed to get canvas context");
+    return;
+  }
+
+  context.putImageData(image, 200, 200);
+};
--- src/hooks/tools/useBlur.ts
+++ src/hooks/tools/useBlur.ts
@@ -1,169 +1,70 @@
 import Konva from "konva";
 import { useAppStore } from "../../store/app";
 import { watch } from "vue";
-
-/*
-1.mousedown: 获取坐标 -> 判断坐标是否在图片内部 -> 创建图片蒙版 -> 显示cricle
-2.mousemove: cricle跟随鼠标移动 -> group的clipFunc根据crilcle路径剪切图片 -> group图片模糊
-*/
+import { gaussBlur } from "../gaussBlur";
 
 export const useBlur = (stage: Konva.Stage, layer: Konva.Layer) => {
   const appStore = useAppStore();
   if (appStore.tool !== "blur") return;
-  let isDrawing  = false;
-  let currentColor = 'black'; 
-  // let blurCircle: Konva.Circle | null = null;
-  let blurLine: Konva.Line | null = null;
-
-
-  const averageRgb = (x:number,y:number) => {
-    x = Math.floor(x);
-    y = Math.floor(y);
-    let totalR = 0;
-    let totalG = 0;
-    let totalB = 0;
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
-        const pixel = layer.getContext().getImageData(x+i, y+j, 1, 1).data;
-        totalR += pixel[0];
-        totalG += pixel[1];
-        totalB += pixel[2];
-      }
-    }
-    let centerR = totalR / 100;
-    let centerG = totalG / 100;
-    let centerB = totalB / 100;
-    return `rgba(${centerR}, ${centerG}, ${centerB},0.6)`;
-  }
-
-
-  const createBlur = () => {
-    const pos = stage.getPointerPosition();
-    if (!pos) return;
-    const x = pos.x;
-    const y = pos.y;
-    currentColor = averageRgb(x,y);
-    // const pixel = layer.getContext().getImageData(pos.x, pos.y, 1, 1).data;
-    // currentColor = `rgba(${pixel[0]}, ${pixel[1]}, ${pixel[2]},0.6)`;
-    blurLine = new Konva.Line({
-      points: [x, y],
-      stroke: currentColor,
-      strokeWidth: 20,
-      lineCap: 'round',
+  let isDrawing = false;
+  let activeImage: Konva.Image | null = null;
+  let imageData: ImageData | null = null;
+  const image = new Image();
+  image.src = "src/assets/test.png";
+  image.onload = () => {
+    const img = new Konva.Image({
+      x: 200,
+      y: 200,
+      image: image,
+      width: 300,
+      height: 300,
     });
-    layer.add(blurLine);
-    layer.batchDraw();
-  }
+    layer.add(img);
+    layer.draw();
 
+    layer.children.forEach((child) => {
+      if (!(child instanceof Konva.Image)) return;
+      child.on("mousedown", (event) => {
+        event.cancelBubble = true;
+        isDrawing = true;
+        activeImage = child;
+        imageData = child
+          .getLayer()!
+          .getContext()
+          .getImageData(img.x(), img.y(), child.width(), child.height());
+        activeImage.on("mousemove", mousemove);
+        activeImage.on("mouseup", mouseup);
+        gaussBlur(activeImage, imageData, 10, 10);
+      });
+    });
+  };
 
-  for (const child of layer.children) {
-    child.on('mousedown', (e) => {
-     if (child.className !== 'Image' || isDrawing) return;
-    //  e.cancelBubble = true;
-     child.draggable(false);
-     isDrawing  = true;
-     createBlur();
-    })
+  const mousemove = () => {
+    if (!isDrawing) return;
+    if (!activeImage) return;
+    if (!imageData) return;
+    gaussBlur(activeImage, imageData, 10, 10);
+  };
 
-    // child.on('mouseup', (e) => {
-    //   console.log('mouseup')
-    //   // e.cancelBubble = true;
-    //   isDrawing  = false;
-    //   blurLine = null;
-    // })
-
-    child.on('mousemove', (e) => {
-      if (!isDrawing ) return;
-      console.log(isDrawing)
-      // e.cancelBubble = true;
-      // 获取鼠标位置
-      const pos = stage.getPointerPosition();
-      if (!pos || !blurLine) return;
-      // const pixel = layer.getContext().getImageData(pos.x, pos.y, 1, 1).data;
-      // currentColor = `rgba(255,255,255,0.6)`;
-      currentColor = averageRgb(pos.x,pos.y);
-      const newPoints = blurLine.points().concat([pos.x, pos.y]);
-      blurLine.points(newPoints);
-      blurLine.cache();
-      blurLine.filters([Konva.Filters.Blur])
-      blurLine.blurRadius(10)
-      layer.batchDraw();
-    })
-    
-  }
-
-
-  stage.on('mouseup', () => {
-    isDrawing  = false;
-    blurLine = null;
-  })
+  const mouseup = () => {
+    isDrawing = false;
+  };
 
   const cancelListener = () => {
-    for (const child of layer.children) {
-      if (child.className !== 'Image') return;
-      child.off("mousedown")
-    }
-  }
+    isDrawing = false;
+    activeImage = null;
+    layer.children.forEach((child) => {
+      if (!(child instanceof Konva.Image)) return;
+      child.off("mousedown");
+      child.off("mousemove", mousemove);
+      child.off("mouseup", mouseup);
+    });
+  };
 
   watch(
     () => appStore.tool,
     () => {
-      // cancelListener()
-      
+      cancelListener();
     }
-  )
-
-
-  // const imageObj = new Image();
-  // imageObj.src = "src/assets/test.png"
-  // imageObj.onload = () => {
-  //   const image = new Konva.Image({
-  //     x:0,
-  //     y:0,
-  //     image: imageObj,
-  //     draggable: false,
-  //     width: stage.width(),
-  //     height: stage.height(),
-  //   })
-  //   layer.add(image)
-  //   layer.draw()
-  // }
-  // let isDrawing  = false;
-
-
-  // let currentColor = 'black'; 
-  // stage.on("mousedown", (e) => {
-  //   isDrawing  = true;
-  
-  // })
-
-  // stage.on('mouseup', () => {
-  //   isDrawing  = false;
-  // });
-
-  // stage.on('mousemove', (e) => {
-  //   if (!isDrawing ) return;
-    
-  //   // 获取鼠标位置
-  //   const pos = stage.getPointerPosition();
-  //   if (!pos) return;
-  //   const pixel = layer.getContext().getImageData(pos.x, pos.y, 1, 1).data;
-  //   currentColor = `rgba(${pixel[0]}, ${pixel[1]}, ${pixel[2]},0.3)`;
-    
-  //   // 创建模糊效果
-  //   const blurCircle = new Konva.Circle({
-  //     x: pos.x,
-  //     y: pos.y,
-  //     radius: 5,
-  //     fill: currentColor, // 可以自定义颜色和透明度
-  //     listening: false, // 不需要监听事件
-  //   });
-    
-  //   layer.add(blurCircle);
-  //   layer.batchDraw();
-  // });
-
- 
-
- 
+  );
 };
